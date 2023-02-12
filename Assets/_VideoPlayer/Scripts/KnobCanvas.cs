40a50e6fcc09a2c9e9cc70506a98dc02f023d802
Hosu~NyanPasu <vortex.k1@gmail.com>
2023-02-13 01:27:23 +0530

    Initial Commit

---
+++ Assets/_VideoPlayer/Scripts/KnobCanvas.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+// [RequireComponent(typeof(MeshCollider))]
+/*
+
+   .-------.                             .--.    .-------.     .--.            .--.     .--.        
+   |       |--.--.--------.-----.-----.--|  |    |_     _|--.--|  |_.-----.----|__|---.-|  |-----.
+   |   -   |_   _|        |  _  |     |  _  |      |   | |  |  |   _|  _  |   _|  |  _  |  |__ --|
+   |_______|__.__|__|__|__|_____|__|__|_____|      |___| |_____|____|_____|__| |__|___._|__|_____|
+   © 2019 OXMOND / www.oxmond.com 
+
+*/
+
+public class KnoKnobCanvasb : MonoBehaviour
+{
+    public GameObject videoPlayer;
+    private MyVideoPlayerCanvas videoPlayerScript;
+
+    void Start()
+    {
+        videoPlayerScript = videoPlayer.GetComponent<MyVideoPlayerCanvas>();
+    }
+
+    void OnMouseDown()
+    {
+        videoPlayerScript.KnobOnPressDown();
+    }
+
+    void OnMouseUp()
+    {
+        videoPlayerScript.KnobOnRelease();
+    }
+
+    void OnMouseDrag()
+    {
+        videoPlayerScript.KnobOnDrag();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# NDITab
- 
+# Accenture-MultitactionValueTree-URP-
+Tab Application for NDI 
+PC app Link -to be added-
